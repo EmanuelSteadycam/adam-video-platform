6bc814017dff713e5eec4598556f5f285ac32c09
EmanuelSteadycam <107688740+EmanuelSteadycam@users.noreply.github.com>
2025-10-06 15:33:28 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useMemo, useRef, useEffect } from 'react';
 import Lottie from 'lottie-react';
-import logoAnimation from './logo-animation.json';
 import { Search, Upload, User, PlayCircle, Clock, Calendar, Eye, School, X, LogOut, Video, ChevronLeft, ChevronRight, Shuffle, Menu } from 'lucide-react';
 import { videos as videosData } from './videosData';
 
